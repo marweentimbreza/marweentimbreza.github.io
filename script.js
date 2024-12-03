461a44a253f40c8fad79088bd2a6508f598b9c8f
Mj Timbreza <marweenjosephtimbreza@gmail.com>
2024-12-03 20:51:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }   
-    })            
-});            
+const sparksContainer = document.querySelector('.sparks');
+
+function createSpark() {
+  const spark = document.createElement('div');
+  spark.classList.add('spark');
+  spark.style.left = `${Math.random() * 50}px`;
+  spark.style.top = `${Math.random() * 20}px`;
+  sparksContainer.appendChild(spark);
+
+  setTimeout(() => spark.remove(), 500);
+}
+
+setInterval(createSpark, 200);
